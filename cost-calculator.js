54831c0b2808c8f62eb714759c735640475102f8
Adrian Stabiszewski <as@grundid.de>
2018-03-03 12:47:02 +0100

    prepared test

--- cost-calculator.js
+++ cost-calculator.js
@@ -1,5 +1,16 @@
-function calcCost() {
+function calcWeekDayCost(tarif, weekDayTarif, request) {
+    const hourOfDay = request.fromTime.getHours();
+    
     return 0;
+}
+
+
+function calcCost(tarif, request) {
+    if (tarif.All) {
+        return calcWeekDayCost(tarif, tarif.All, request);
+    } else {
+        return 0;
+    }
 }
 
 module.exports = {calcCost};
