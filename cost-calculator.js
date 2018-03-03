71c867c73d7b0464156b8b7b7388a54864902100
Adrian Stabiszewski <as@grundid.de>
2018-03-03 14:08:43 +0100

    basic calculation added

--- cost-calculator.js
+++ cost-calculator.js
@@ -1,15 +1,45 @@
+const moment = require("moment");
+
 function calcWeekDayCost(tarif, weekDayTarif, request) {
-    const hourOfDay = request.fromTime.getHours();
-    
-    return 0;
+    const parkStart = moment(request.fromTime);
+    const parkEnd = moment(request.fromTime).add(request.duration, "minutes");
+
+    const context = {
+        cost: 0,
+        parkDuration: 0
+    };
+
+    while (parkStart.isBefore(parkEnd)) {
+        //console.log("parkStart: ", parkStart);
+        //console.log("parkEnd: ", parkEnd);
+
+        const hourOfDay = parkStart.hour();
+        const feeForGivenHour = weekDayTarif.find((element) => {
+            return hourOfDay >= element.from && hourOfDay < element.to;
+        });
+
+        const fee = feeForGivenHour.fees.find((element) => {
+            return context.parkDuration >= element.parkTime.from && context.parkDuration < element.parkTime.to;
+        });
+
+        context.cost += fee.fee;
+        context.parkDuration += fee.unit;
+
+        parkStart.add(fee.unit, "minutes");
+    }
+
+    return {cost: context.cost, endTime: parkStart};
 }
 
 
 function calcCost(tarif, request) {
-    if (tarif.All) {
-        return calcWeekDayCost(tarif, tarif.All, request);
+    if (tarif.costs.ALL) {
+        return calcWeekDayCost(tarif, tarif.costs.ALL, request);
     } else {
-        return 0;
+        const fromTime = moment(request.fromTime);
+        const dow = fromTime.format("ddd");
+        const dowTarif = tarif.costs[dow];
+        return calcWeekDayCost(tarif, dowTarif, request);
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -8,6 +8,27 @@ describe("test", () => {
             fromTime: new Date(2018, 2, 3, 15, 0, 0),
             duration: 150
         };
-        assert.equal(costCalculator.calcCost(tarifs.kiliansplatz,  request), 0);
+        assert.equal(costCalculator.calcCost(tarifs["Käthchenhof"],  request).cost, 4);
+    });
+    it("should calculate cost for 150 mins kiliansplatz on saturday", () => {
+        const request = {
+            fromTime: new Date(2018, 2, 3, 15, 0, 0),
+            duration: 150
+        };
+        assert.equal(costCalculator.calcCost(tarifs["kiliansplatz"],  request).cost, 8);
+    });
+    it("should calculate cost for 150 mins kiliansplatz on friday", () => {
+        const request = {
+            fromTime: new Date(2018, 2, 2, 15, 0, 0),
+            duration: 150
+        };
+        assert.equal(costCalculator.calcCost(tarifs["kiliansplatz"],  request).cost, 6);
+    });
+    it("should calculate cost for 150 mins kiliansplatz evening", () => {
+        const request = {
+            fromTime: new Date(2018, 2, 2, 20, 0, 0),
+            duration: 150
+        };
+        assert.equal(costCalculator.calcCost(tarifs["kiliansplatz"],  request).cost, 5);
     });
 });
